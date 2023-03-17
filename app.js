c4c77aaa58ae4eecaf185c87ac3cd2c8ce945bbd
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-17 21:12:12 +0600

    first problem solve

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ const showImages = (images) => {
 const getImages = (query) => {
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hitS))
+    .then(data => showImages(data.hits))
+    // .then(data => console.log(data))
     .catch(err => console.log(err))
 }
 
@@ -39,7 +40,7 @@ let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
   element.classList.add('added');
- 
+
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
